8919551d7513848f7961859e76af965dfa249e4d
tursunov-r <tursunov.rus.developer@gmail.com>
2026-06-29 01:09:57 +0500

    add permision check

--- src/api/admin_handlers.py
+++ src/api/admin_handlers.py
@@ -7,7 +7,6 @@ from src.schemas.admin_schemas import AdminUserDataResponseSchema, AdminUserCrea
     AdminUserUpdateSchema
 from src.schemas.user_schemas import TokenData
 from src.services.admin_service import admin_service
-from src.utils.auth import get_current_user
 from src.utils.require_admin import require_admin
 
 router = APIRouter(prefix="/api/v1/admin_handlers/users", tags=["admin"])
@@ -15,7 +14,7 @@ router = APIRouter(prefix="/api/v1/admin_handlers/users", tags=["admin"])
 
 @router.post("/", response_model=AdminUserCreateResponseSchema, status_code=201)
 async def create_user(user: AdminUserCreateSchema, session: AsyncSession = Depends(get_session),
-                      token: TokenData = Depends(get_current_user)):
+                      token: TokenData = Depends(require_admin)):
     result = await admin_service.create_user(user=user, session=session, token=token)
     return {"message": "create success", "data": result}
 
@@ -23,7 +22,7 @@ async def create_user(user: AdminUserCreateSchema, session: AsyncSession = Depen
 @router.get("/", response_model=list[AdminUserDataResponseSchema])
 async def get_users(
         session: AsyncSession = Depends(get_session),
-        token: TokenData = Depends(get_current_user),
+        token: TokenData = Depends(require_admin),
 ):
     users = await admin_service.get_users(session=session, token=token)
     return users
@@ -31,19 +30,19 @@ async def get_users(
 
 @router.get("/{user_id}")
 async def get_user_by_id(user_id: int, session: AsyncSession = Depends(get_session),
-                         token: TokenData = Depends(get_current_user)):
+                         token: TokenData = Depends(require_admin)):
     result = await user_repository.get_user_by_id(user_id=user_id, session=session)
     return result
 
 
 @router.patch("/", response_model=AdminUserCreateResponseSchema)
 async def update_user(user: AdminUserUpdateSchema, session: AsyncSession = Depends(get_session),
-                      token: TokenData = Depends(get_current_user)):
+                      token: TokenData = Depends(require_admin)):
     updated_user = await admin_service.update_user(user=user, session=session, token=token)
     return {"message": "update success", "data": updated_user}
 
 
 @router.delete("/{user_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_user(user_id: int, session: AsyncSession = Depends(get_session)):
+async def delete_user(user_id: int, session: AsyncSession = Depends(require_admin)):
     await admin_service.delete_user(user_id=user_id,session=session)
     return status.HTTP_204_NO_CONTENT
--- src/utils/require_admin.py
+++ src/utils/require_admin.py
@@ -1,8 +1,11 @@
+from fastapi import Depends
+
 from src.exceptions.auth_exceptions import Forbidden
 from src.schemas.user_schemas import TokenData
+from src.utils.auth import get_current_user
 
 
-def require_admin(user: TokenData):
+def require_admin(user: TokenData = Depends(get_current_user)):
     """Функция для проверки прав"""
     if user.role == "admin":
         return user
